b2774af189862a58ec105a671518d4a64e31854c
TheCrafter4000 <thecrafter4000.mc@gmail.com>
2018-08-02 11:51:37 +0200

    Bump to 1.0.6.1

--- src/main/java/thecrafter4000/unlimitedchat/UnlimitedChat.java
+++ src/main/java/thecrafter4000/unlimitedchat/UnlimitedChat.java
@@ -19,7 +19,7 @@ import org.apache.logging.log4j.Logger;
 @Mod(modid = UnlimitedChat.MODID, version = UnlimitedChat.VERSION, name = UnlimitedChat.NAME, acceptedMinecraftVersions = "1.7.10" )
 public class UnlimitedChat {
 	public static final String MODID = "unlimitedchat";
-	public static final String VERSION = "1.0.6";
+	public static final String VERSION = "1.0.6.1";
 	public static final String NAME = "UnlimitedChat";
 	
 	@Instance
