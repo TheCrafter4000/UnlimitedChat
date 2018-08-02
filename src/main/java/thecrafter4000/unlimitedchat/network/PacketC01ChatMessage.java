dce2c017694ab958359329698242aa3e206bf568
TheCrafter4000 <thecrafter4000.mc@gmail.com>
2018-08-02 11:50:18 +0200

    Fixed bug with links

--- src/main/java/thecrafter4000/unlimitedchat/network/PacketC01ChatMessage.java
+++ src/main/java/thecrafter4000/unlimitedchat/network/PacketC01ChatMessage.java
@@ -6,18 +6,20 @@ import cpw.mods.fml.common.network.simpleimpl.IMessageHandler;
 import cpw.mods.fml.common.network.simpleimpl.MessageContext;
 import io.netty.buffer.ByteBuf;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.event.ClickEvent;
 import net.minecraft.network.NetHandlerPlayServer;
 import net.minecraft.network.play.server.S02PacketChat;
 import net.minecraft.server.MinecraftServer;
-import net.minecraft.util.ChatAllowedCharacters;
-import net.minecraft.util.ChatComponentTranslation;
-import net.minecraft.util.EnumChatFormatting;
+import net.minecraft.util.*;
 import net.minecraftforge.common.ForgeHooks;
 import thecrafter4000.unlimitedchat.ServerProxy;
 import thecrafter4000.unlimitedchat.UnlimitedChat;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+import java.net.URI;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * A packet sent form client to server containing a chat message.
@@ -61,9 +63,9 @@ public class PacketC01ChatMessage implements IMessage {
 		
 		private void processChatMessage(NetHandlerPlayServer nhps, String text) throws Exception {
 			
-			// Prevent chating while chat gui is not open.
+			// Prevent chatting while chat gui is not open.
 			if (nhps.playerEntity.func_147096_v() == EntityPlayer.EnumChatVisibility.HIDDEN) { 
-				ChatComponentTranslation chatcomponenttranslation = new ChatComponentTranslation("chat.cannotSend", new Object[0]);
+				ChatComponentTranslation chatcomponenttranslation = new ChatComponentTranslation("chat.cannotSend");
 				chatcomponenttranslation.getChatStyle().setColor(EnumChatFormatting.RED);
 				nhps.sendPacket(new S02PacketChat(chatcomponenttranslation));
 				return;
@@ -89,12 +91,11 @@ public class PacketC01ChatMessage implements IMessage {
 			if (text.startsWith("/")) {
 				this.executeCommand().invoke(nhps, text);
 			}
-			else
-			{
-				ChatComponentTranslation chatcomponenttranslation1 = new ChatComponentTranslation("chat.type.text", new Object[] {nhps.playerEntity.func_145748_c_(), ForgeHooks.newChatWithLinks(text)}); // Fixes chat links
-				chatcomponenttranslation1 = ForgeHooks.onServerChatEvent(nhps, text, chatcomponenttranslation1);
-				if (chatcomponenttranslation1 == null) return;
-				MinecraftServer.getServer().getConfigurationManager().sendChatMsgImpl(chatcomponenttranslation1, false); // Send's the message to all clients
+			else {
+				ChatComponentTranslation chatComponentTranslation = new ChatComponentTranslation("chat.type.text", nhps.playerEntity.func_145748_c_(), newChatWithLinks(text)); // Fixes chat links
+				chatComponentTranslation = ForgeHooks.onServerChatEvent(nhps, text, chatComponentTranslation);
+				if (chatComponentTranslation == null) return;
+				MinecraftServer.getServer().getConfigurationManager().sendChatMsgImpl(chatComponentTranslation, false); // Send's the message to all clients
 			}
 
 			if(!ServerProxy.getProperties(nhps.playerEntity).ignoreSpamCheck) { // Disables spam check
@@ -132,5 +133,49 @@ public class PacketC01ChatMessage implements IMessage {
 			return m;
 		}
 
+		// Forge fix - Original see ForgeHooks#newChatWithLinks(String)
+
+		// Includes ipv4 and domain pattern
+		// Matches an ip (xx.xxx.xx.xxx) or a domain (something.com) with or
+		// without a protocol or path.
+		private static final Pattern URL_PATTERN = Pattern.compile(
+				//         schema                          ipv4            OR           namespace                 port     path         ends
+				//   |-----------------|        |-------------------------|  |----------------------------|    |---------| |--|   |---------------|
+				"((?:[a-z0-9]{2,}://)?(?:(?:[0-9]{1,3}\\.){3}[0-9]{1,3}|(?:[-\\w_.]+\\.[a-z]{2,}?))(?::[0-9]{1,5})?.*?(?=[!\"\u00A7 \n]|$))",
+				Pattern.CASE_INSENSITIVE);
+
+		public static IChatComponent newChatWithLinks(String string) {
+			IChatComponent chatComponent = new ChatComponentText("");
+			Matcher matcher = URL_PATTERN.matcher(string);
+			int lastEnd = 0;
+
+			// Find all urls
+			while (matcher.find()) {
+				int start = matcher.start();
+				int end = matcher.end();
+
+				// Append the previous left overs.
+				chatComponent.appendText(string.substring(lastEnd, start));
+				lastEnd = end;
+				String url = string.substring(start, end);
+				IChatComponent link = new ChatComponentText(url);
+
+				try{
+					// Add schema so client doesn't crash.
+					if (URI.create(url).getScheme() == null) {
+						url = "http://" + url;
+					}
+				}catch (IllegalArgumentException ignored){} // Catch exception in case of invalid url, e.g. xx.xx.xx.xx:xxxxx
+
+				// Set the click event and append the link.
+				ClickEvent click = new ClickEvent(ClickEvent.Action.OPEN_URL, url);
+				link.getChatStyle().setChatClickEvent(click);
+				chatComponent.appendSibling(link);
+			}
+
+			// Append the rest of the message.
+			chatComponent.appendText(string.substring(lastEnd));
+			return chatComponent;
+		}
 	}
 }
